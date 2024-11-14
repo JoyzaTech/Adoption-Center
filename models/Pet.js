56816880e70299614b1b02c9f720628a1057e47f
Nico <141285419+red4cted25@users.noreply.github.com>
2024-11-14 13:33:45 -0700

    Fixed Logo and Surrender Page
    Fixed the logo and surrender page so that images work for the site and the surrender page can properly upload pets using the schema

--- models/Pet.js
+++ models/Pet.js
@@ -1,1 +1,53 @@
-// Pet schema
+const mongoose = require('mongoose');
+
+const PetSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Must provide pet name'],
+        trim: true,
+    },
+    gender: {
+        type: String,
+        required: [true, 'Must provide gender'],
+        enum: {
+            values: ['male', 'female', 'unknown'],
+            message: '{VALUE} is not supported'
+        }
+    },
+    breed: {
+        type: String,
+        required: [true, 'Must provide breed'],
+        trim: true,
+    },
+    age: {
+        type: Number,
+        required: [true, 'Must provide age'],
+    },
+    description: {
+        type: String,
+        required: [true, 'Must provide description'],
+        trim: true,
+    },
+    behavior: {
+        type: String,
+        required: [true, 'Must provide behavior'],
+        trim: true,
+    },
+    history: {
+        type: String,
+        required: [true, 'Must provide history'],
+        trim: true,
+    },
+    location: {
+        type: String,
+        required: [true, 'Must provide location'],
+        trim: true,
+    },
+    imageUrl: {
+        type: String,
+        required: [true, 'Must provide image URL'],
+        trim: true,
+    }
+}, {collection: 'pets'});
+
+module.exports = mongoose.model('Pet', PetSchema);
